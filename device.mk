ea2f19b832cd6c841e2446735bf2958f9f228529
Akash98Sky <a98mondal@gmail.com>
2020-11-08 02:44:29 +0530

    tulip: Build power stats hal & label it
    * Fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Co-authored-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: Arturo <rtsanonynousperson@gmail.com>

--- device.mk
+++ device.mk
@@ -296,7 +296,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.tulip-libperfmgr
+    android.hardware.power-service.tulip-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 PRODUCT_PACKAGES += \
