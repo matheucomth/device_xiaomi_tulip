1c9ae33ae436ad31c39e042012f28b6d5c794333
TheSanty <sudhiryadav.igi@gmail.com>
2022-06-30 18:05:25 -0300

    tulip: Build QTI Perf Vendor Variant
    06-24 01:10:45.394 F/linker  (12177): CANNOT LINK EXECUTABLE
    "/vendor/bin/hw/vendor.qti.hardware.perf@2.2-service":
    library "vendor.qti.hardware.perf@2.0.so" not found: needed by main executable
    ---

--- device.mk
+++ device.mk
@@ -337,7 +337,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
-    libqti-perfd-client
+    libqti-perfd-client \
+	vendor.qti.hardware.perf@2.2.vendor \
+	vendor.qti.hardware.perf@2.0.vendor \
+	vendor.qti.hardware.perf@2.1.vendor
+	
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
